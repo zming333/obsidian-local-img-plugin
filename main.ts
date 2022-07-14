2da3297cb5abd763f5b8d576771f70fbb0a3b7f0
talengu <talenapp@gmail.com>
2022-07-15 01:20:35 +0800

    update

--- main.ts
+++ main.ts
@@ -2,25 +2,31 @@ import {Platform, Plugin, TFile} from "obsidian";
 import type { MarkdownView } from 'obsidian';
 import {platform} from "os";
 
+console.log("Running on sample img")
 export default class HtmlLocalSrcPlugin extends Plugin {
   async onload() {
+	console.log("Running on ---------------onload")
     this.registerMarkdownPostProcessor((element, ctx) => {
-
+		console.log("Running on ===1")
+		const active_file = this.app.workspace.getActiveFile();
+		console.log(active_file.basename);
       const targetLinks = Array.from(element.getElementsByTagName("img")).filter(
         (link) =>
-          link.src.contains("_media")
+          link.src.contains(active_file.basename)
       );
-      const active_file = this.app.workspace.getActiveFile();
+      
+	  
       let active_path = this.app.vault.getResourcePath(active_file)
       active_path = active_path.substring(0, active_path.lastIndexOf("/"));
-      //console.log('active_file_path: ' + active_path)
+      console.log('active_file_path: ' + active_path)
+	  console.log('arg'+targetLinks)
       for (const link of targetLinks) {
         let clean_link = link.src.replace('app://obsidian.md/', '')
         // For iOS
         clean_link = clean_link.replace('capacitor://localhost/', '')
-        //console.log('clean_link: ' + clean_link)
+        console.log('clean_link: ' + clean_link)
         let full_link =  active_path + '/' + clean_link
-        //console.log('full_link: ' + full_link)
+        console.log('full_link: ' + full_link)
         link.src = full_link
         if(Platform.isMobile) {
           console.log("Running on mobile platform - setting object fit and height of img")
